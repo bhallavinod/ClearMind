a39f7854c1c3ca2a88ac53b358cdba7416bb4215
bhallavinod <bhallavinod@gmail.com>
2026-03-27 18:41:43 +0530

    Update CACHE_NAME from 'zen-v1' to 'zen-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zen-v1';
+const CACHE_NAME = 'zen-v2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './pashupati.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
